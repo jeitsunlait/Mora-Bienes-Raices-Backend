b46c9e640df0c830402e1493113101c8bc34fcee
jeitsunlait <jerielalferezsilva@gmail.com>
2026-04-20 04:45:04 -0600

    KAN-8 base-de-datos.sql

--- base-de-datos.sql
+++ base-de-datos.sql
@@ -1,1 +1,10 @@
 CREATE TABLE casas (id INT, direccion VARCHAR(255));
+CREATE TABLE propiedades (
+    id_propiedad INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    precio DECIMAL(12, 2) NOT NULL,
+    ubicacion VARCHAR(255),
+    tipo_operacion ENUM('Venta', 'Renta'), 
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
