65b87612c1ffb7b327c218dcdb79ccdb65466a5e
jeitsunlait <jerielalferezsilva@gmail.com>
2026-04-20 04:46:15 -0600

    git commit -m "KAN-2 Diseño de la base de datos"

--- base-de-datos.sql
+++ base-de-datos.sql
@@ -3,7 +3,7 @@ CREATE TABLE propiedades (
     id_propiedad INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(100) NOT NULL,
     descripcion TEXT,
-    precio DECIMAL(12, 2) NOT NULL,
+    precio DECIMAL(12, 1) NOT NULL,
     ubicacion VARCHAR(255),
     tipo_operacion ENUM('Venta', 'Renta'), 
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
